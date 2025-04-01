0486330d16f9571c62ea7bb9bddc64e306b5abfc
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2025-04-01 15:31:04 -0500

    version 0.0.2

--- data/cursos-sgsst-carousel1-info.php
+++ data/cursos-sgsst-carousel1-info.php
@@ -18,7 +18,8 @@ for($i = 2;$i<count($cursos);$i++){
 }
 
 $carousel_data = array(
-    'link'=>'cursos-detalles.php?course=',
+    //'link'=>'cursos-detalles.php?course=',
+    'link'=>'https://docs.google.com/document/d/1voqkGi6tMf-qpZxe8GyUPgBPSFMIi1ahJzE6FgLcjfo/edit?usp=sharing'
     'items'=>$items
 );
 
--- includes/components/carousel/carousel.php
+++ includes/components/carousel/carousel.php
@@ -5,7 +5,7 @@
         <div class="app-carousel-wrap" page="0" total="<?php echo count($carousel_data['items'])?>">
             <div class="app-carousel-wrapper">
                 <?php for($i = 0;$i<count($carousel_data['items']);$i++){ ?>
-                    <a class="app-carousel-row" href="<?php echo $carousel_data['link'].$carousel_data['items'][$i]['id']?>" target="_blank" style="width: calc(100% / <?php echo count($carousel_data['items'])?>);">
+                    <a class="app-carousel-row" href="<?php echo $carousel_data['link']?>" target="_blank" style="width: calc(100% / <?php echo count($carousel_data['items'])?>);">
                         <div class="app-carousel-item">
                             <div style="background-image: url(<?php echo $carousel_data['items'][$i]['thumbnail']?>)"></div>
                             <h4><?php echo $carousel_data['items'][$i]['title']?></h4>
