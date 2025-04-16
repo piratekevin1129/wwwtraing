63e03756e6a3e597758d1c9f5f1d0522104fe419
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2025-04-16 02:59:43 -0500

    version 0.0.8

---
+++ contenidos.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php include 'includes/styles.php'?>
+    <link href="assets/css/contenidos.css" rel="stylesheet" type="text/css" />
+    
+    <title>Traing - Contenidos</title>
+</head>
+<body>
+    <?php include 'includes/header.php' ?>
+
+    <div class="banner">
+        <img src="assets/images/contenidos/banner.jpg" />
+    </div>
+    <div class="bannertip">
+        <div class="bannertip-box">
+            <div class="bannertip-content">
+                <h1><span>Capacitaciones</span> creativas, efectivas y personalizadas</h1>
+                <p>Tenemos opciones infinitas para encontrar la mejor alternativa a partir de las necesidades especificas de tu empresa. cada caso es diferente y merece esa solución que estás esperando.</p>
+            </div>
+        </div>
+        <div class="bannertip-img">
+            <img src="assets/images/contenidos/banner-tip-icon.svg" />
+        </div>
+    </div>
+
+    <div class="container py-100">
+        <h1 class="main-title-2">¿Cómo producimos tus contenidos e-learning a medida?</h1>
+
+        <br>
+        <br>
+
+        <div class="contenidos-columna">
+            <div class="contenidos-row">
+                <div class="contenido-item">
+                    <div class="contenido-icono">
+                        <img src="assets/images/contenidos/escuchamos.svg" />
+                    </div>
+                    <div class="contenido-number">
+                        <p>1</p>
+                    </div>
+                    <div class="contenido-texto">
+                        <p><span>Escuchamos</span> tus necesidades de formación</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="contenidos-row">
+                <div class="contenido-item">
+                    <div class="contenido-icono">
+                        <img src="assets/images/contenidos/construimos.svg" />
+                    </div>
+                    <div class="contenido-number">
+                        <p>2</p>
+                    </div>
+                    <div class="contenido-texto">
+                        <p><span>Construimos</span> un diseño instruccional</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="contenidos-row">
+                <div class="contenido-item">
+                    <div class="contenido-icono">
+                        <img src="assets/images/contenidos/realizamos.svg" />
+                    </div>
+                    <div class="contenido-number">
+                        <p>3</p>
+                    </div>
+                    <div class="contenido-texto">
+                        <p><span>Realizamos</span> la producción del contenido e-learning</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="contenidos-row">
+                <div class="contenido-item">
+                    <div class="contenido-icono">
+                        <img src="assets/images/contenidos/publicamos.svg" />
+                    </div>
+                    <div class="contenido-number">
+                        <p>4</p>
+                    </div>
+                    <div class="contenido-texto">
+                        <p><span>Publicamos</span> una muestra de avance</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="contenidos-row">
+                <div class="contenido-item">
+                    <div class="contenido-icono">
+                        <img src="assets/images/contenidos/entregamos.svg" />
+                    </div>
+                    <div class="contenido-number">
+                        <p>5</p>
+                    </div>
+                    <div class="contenido-texto">
+                        <p><span>Entregamos</span> el proyecto e-learning a satisfacción</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-black py-50">
+        <div class="proyectos-header">
+            <p class="submain-text"><span>Algunos</span> Proyectos</p>
+            <p class="submain-text-2">Convertimos cualquier contenido en una experiencia de formación efectiva y visualmente atractiva.</p>
+
+            <div class="proyectos-container">
+                <div class="proyectos-header">
+                    <div class="proyectos-header-item proyectos-header-item-selected">
+                        <p>TODO</p>
+                    </div>
+                    <div class="proyectos-header-item">
+                        <p>E-LEARNING</p>
+                    </div>
+                    <div class="proyectos-header-item">
+                        <p>GAMIFICACIÓN</p>
+                    </div>
+                    <div class="proyectos-header-item">
+                        <p>CARTILLAS</p>
+                    </div>
+                </div>
+
+                <div class="proyectos-body">
+                    <div class="proyectos-body-item">
+                        <div class="proyectos-body-col">
+                            <div class="proyectos-body-row">
+                                <div class="proyecto-card">
+                                    <div class="proyecto-card-img">
+                                        <img src="" />
+                                    </div>
+                                    <div class="proyecto-card-info">
+                                        <h4>Estandar</h4>
+                                        <p>Creación e integración de ilustraciones, en VideoScribe.</p>
+                                    </div>
+                                </div>
+                                <div class="proyecto-card-reflejo"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php' ?>
+</body>
+</html>
